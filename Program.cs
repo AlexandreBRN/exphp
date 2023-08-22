72622192785fb92aad11df7e685a95e78e369fbd
AlexandreBR <130804109+AlexandreBRN@users.noreply.github.com>
2023-08-22 14:43:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+//namespace ExemploIdade
+//{
+    //class Program
+    //{
+        //static void Main(string[] args)
+        //{
+            //int idade = 35;
+            //Console.WriteLine(idade);
+        //}
+    //}
+//}
+
+//namespace ExemploNome
+//{
+    //class Program
+    //{
+        //static void Main(string[] args)
+        //{
+            //string nome = "Maria";
+            //Console.WriteLine(nome);
+        //}
+    //}
+//}
+
+
+//namespace ExemploAltura
+//{
+    //class Program
+    //{
+        //static void Main(string[] args)
+        //{
+            //double altura = 3.45;
+            //Console.WriteLine(altura);
+        //}
+    //}
+//}
+
+
+//namespace ExemploData
+//{
+    //class Program
+    //{
+        //static void Main(string[] args)
+        //{
+            //string data = "04/09/1999";
+            //Console.WriteLine(data);
+        //}
+    //}
+//}
+
+
+//namespace ExemploConstante
+//{
+    //class Program
+    //{
+        //static void Main(string[] args)
+        //{
+            //const int ano = 12;
+            //Console.WriteLine(ano);
+        //}
+    //}
+//}
+
+
+//namespace ExemploNullable
+//{
+    //class Program
+    //{
+        //static void Main(string[] args)
+        //{
+            //double? nota = 7.80;
+            //Console.WriteLine(nota);
+        //}
+    //}
+//}
+
+
+//namespace ExemploPeso
+//{
+    //class Program
+    //{
+        //static void Main(string[] args)
+        //{
+            //double peso = 68.5;
+            //Console.WriteLine(peso);
+        //}
+    //}
+//}
+
+
+//namespace ExemploCidade
+//{
+    //class Program
+    //{
+        //static void Main(string[] args)
+        //{
+            //string cidade = "São Paulo";
+            //Console.WriteLine(cidade);
+        //}
+    //}
+//}
+
+using System;
+
+//namespace ExemploLargura
+//{
+    //class Program
+    //{
+        //static void Main(string[] args)
+        //{
+            //double largura = 2.20;
+            //Console.WriteLine(largura);
+        //}
+    //}
+//}
+
+
+//namespace ExemploNascimento
+//{
+    //class Program
+    //{
+        //static void Main(string[] args)
+        //{
+            //DateTime nascimento = new DateTime(1985, 10, 12);
+            //Console.WriteLine(nascimento.ToShortDateString());
+        //}
+    //}
+//}
+
+
+//namespace ExemploTemperatura
+//{
+    //class Program
+    //{
+        //static void Main(string[] args)
+        //{
+            //double? temperatura = 22.5;
+            //Console.WriteLine(temperatura);
+        //}
+    //}
+//}
+
+
+
+
